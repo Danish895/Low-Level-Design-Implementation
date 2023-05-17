f87fd1610940d696177331826cf99de6191f40f0
Mohd Danish <dkhan895@gmail.com>
2023-05-17 23:57:12 +0530

    strategy pattern  addition

--- LowLevelDesign/Design_Patterns/Behavioural_Design_Pattern/Strategy design pattern/IShippingStrategy.cs
+++ LowLevelDesign/Design_Patterns/Behavioural_Design_Pattern/Strategy design pattern/IShippingStrategy.cs
@@ -58,6 +58,123 @@ namespace LowLevelDesign.Design_Patterns.Behavioural_Design_Pattern.Strategy_des
     double shippingCost = order.CalculateShippingCost(); // calculates shipping cost based on weight
     order.SetShippingStrategy(new TotalBasedShippingStrategy());
     shippingCost = order.CalculateShippingCost(); // calculates shipping cost based on order total
+
+    public interface IPaymentStrategy
+{
+    void Pay(double amount);
+}
+Step 2: Implement different payment strategies
+
+csharp
+Copy code
+public class CreditCardStrategy : IPaymentStrategy
+{
+    private string cardNumber;
+    private string expiryDate;
+    private string cvv;
+
+    public CreditCardStrategy(string cardNumber, string expiryDate, string cvv)
+    {
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
+    }
+
+    public void Pay(double amount)
+    {
+        // Implement credit card payment logic here
+        Console.WriteLine($"Paid {amount} using credit card {cardNumber}");
+    }
+}
+
+public class PayPalStrategy : IPaymentStrategy
+{
+    private string email;
+    private string password;
+
+    public PayPalStrategy(string email, string password)
+    {
+        this.email = email;
+        this.password = password;
+    }
+
+    public void Pay(double amount)
+    {
+        // Implement PayPal payment logic here
+        Console.WriteLine($"Paid {amount} using PayPal account {email}");
+    }
+}
+
+public class BankTransferStrategy : IPaymentStrategy
+{
+    private string accountNumber;
+    private string bankCode;
+
+    public BankTransferStrategy(string accountNumber, string bankCode)
+    {
+        this.accountNumber = accountNumber;
+        this.bankCode = bankCode;
+    }
+
+    public void Pay(double amount)
+    {
+        // Implement bank transfer logic here
+        Console.WriteLine($"Paid {amount} via bank transfer to account {accountNumber}");
+    }
+}
+Step 3: Use the payment strategies in the client code
+
+csharp
+Copy code
+public class PaymentProcessor
+{
+    private IPaymentStrategy paymentStrategy;
+
+    public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+    {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void ProcessPayment(double amount)
+    {
+        paymentStrategy.Pay(amount);
+    }
+}
+Step 4: Test the implementation
+
+csharp
+Copy code
+class Program
+{
+    static void Main(string[] args)
+    {
+        PaymentProcessor processor = new PaymentProcessor();
+
+        // Credit card payment
+        string cardNumber = "1234567890123456";
+        string expiryDate = "12/2024";
+        string cvv = "123";
+        IPaymentStrategy creditCardStrategy = new CreditCardStrategy(cardNumber, expiryDate, cvv);
+        processor.SetPaymentStrategy(creditCardStrategy);
+        processor.ProcessPayment(100.0);
+
+        // PayPal payment
+        string email = "example@example.com";
+        string password = "password";
+        IPaymentStrategy payPalStrategy = new PayPalStrategy(email, password);
+        processor.SetPaymentStrategy(payPalStrategy);
+        processor.ProcessPayment(50.0);
+
+        // Bank transfer payment
+        string accountNumber = "1234567890";
+        string bankCode = "ABCD";
+        IPaymentStrategy bankTransferStrategy = new BankTransferStrategy(accountNumber, bankCode);
+        processor.SetPaymentStrategy(bankTransferStrategy);
+        processor.ProcessPayment(200.0);
+
+        Console.ReadLine();
+    }
+}
     
      
      */
